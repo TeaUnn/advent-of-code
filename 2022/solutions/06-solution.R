0ca6ef0be806b863ab95c3ab74cfe0b697c50f8c
TeaUnn <tea@folkdatorn.se>
2022-12-06 07:33:41 +0100

    day 6

---
+++ 2022/solutions/06-solution.R
@@ -0,0 +1,20 @@
+setwd("/Users/tea/coding/advent-of-code/2022")
+
+##### 04 #####
+input <- readLines("input/06-input.txt")
+
+### part 1
+input <- strsplit(input, "")
+
+i <- 4
+while (length(unique(input[[1]][(i-3):i])) < 4) {
+  i <- i + 1
+}
+i
+
+### part 2
+i <- 14
+while (length(unique(input[[1]][(i-13):i])) < 14) {
+  i <- i + 1
+}
+i
