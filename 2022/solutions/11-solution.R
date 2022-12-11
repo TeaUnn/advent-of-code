392b47768a187308ebc521ab7ca9977eaa430aba
TeaUnn <tea@folkdatorn.se>
2022-12-11 17:55:03 +0100

    Update 11-solution.R

--- 2022/solutions/11-solution.R
+++ 2022/solutions/11-solution.R
@@ -140,6 +140,9 @@ for (i in 1:10000) {
   if (length(monkey$"0"$item) > 0) {
     for (j in 1:length(monkey$"0"$item)) {
       monkey$"0"$item[j] <- monkey$"0"$item[j] * 11
+      while (monkey$"0"$item[j] >= prod) {
+        monkey$"0"$item[j] <- monkey$"0"$item[j] %% prod
+      }
       monkey$"0"$count <- monkey$"0"$count + 1
       if (monkey$"0"$item[j] %% 7 == 0) {
         monkey$"6"$item <- c(monkey$"6"$item, monkey$"0"$item[j])
@@ -151,6 +154,9 @@ for (i in 1:10000) {
   if (length(monkey$"1"$item) > 0) {
     for (j in 1:length(monkey$"1"$item)) {
       monkey$"1"$item[j] <- monkey$"1"$item[j] * 17
+      while (monkey$"1"$item[j] >= prod) {
+        monkey$"1"$item[j] <- monkey$"1"$item[j] %% prod
+      }
       monkey$"1"$count <- monkey$"1"$count + 1
       if (monkey$"1"$item[j] %% 13 == 0) {
         monkey$"5"$item <- c(monkey$"5"$item, monkey$"1"$item[j])
@@ -162,6 +168,9 @@ for (i in 1:10000) {
   if (length(monkey$"2"$item) > 0) {
     for (j in 1:length(monkey$"2"$item)) {
       monkey$"2"$item[j] <- monkey$"2"$item[j] + 8
+      while (monkey$"2"$item[j] >= prod) {
+        monkey$"2"$item[j] <- monkey$"2"$item[j] %% prod
+      }
       monkey$"2"$count <- monkey$"2"$count + 1
       if (monkey$"2"$item[j] %% 5 == 0) {
         monkey$"4"$item <- c(monkey$"4"$item, monkey$"2"$item[j])
@@ -173,6 +182,9 @@ for (i in 1:10000) {
   if (length(monkey$"3"$item) > 0) {
     for (j in 1:length(monkey$"3"$item)) {
       monkey$"3"$item[j] <- monkey$"3"$item[j] + 3
+      while (monkey$"3"$item[j] >= prod) {
+        monkey$"3"$item[j] <- monkey$"3"$item[j] %% prod
+      }
       monkey$"3"$count <- monkey$"3"$count + 1
       if (monkey$"3"$item[j] %% 19 == 0) {
         monkey$"6"$item <- c(monkey$"6"$item, monkey$"3"$item[j])
@@ -184,6 +196,9 @@ for (i in 1:10000) {
   if (length(monkey$"4"$item) > 0) {
     for (j in 1:length(monkey$"4"$item)) {
       monkey$"4"$item[j] <- monkey$"4"$item[j] + 4
+      while (monkey$"4"$item[j] >= prod) {
+        monkey$"4"$item[j] <- monkey$"4"$item[j] %% prod
+      }
       monkey$"4"$count <- monkey$"4"$count + 1
       if (monkey$"4"$item[j] %% 2 == 0) {
         monkey$"0"$item <- c(monkey$"0"$item, monkey$"4"$item[j])
@@ -195,6 +210,9 @@ for (i in 1:10000) {
   if (length(monkey$"5"$item) > 0) {
     for (j in 1:length(monkey$"5"$item)) {
       monkey$"5"$item[j] <- monkey$"5"$item[j] + 7
+      while (monkey$"5"$item[j] >= prod) {
+        monkey$"5"$item[j] <- monkey$"5"$item[j] %% prod
+      }
       monkey$"5"$count <- monkey$"5"$count + 1
       if (monkey$"5"$item[j] %% 11 == 0) {
         monkey$"3"$item <- c(monkey$"3"$item, monkey$"5"$item[j])
@@ -206,6 +224,9 @@ for (i in 1:10000) {
   if (length(monkey$"6"$item) > 0) {
     for (j in 1:length(monkey$"6"$item)) {
       monkey$"6"$item[j] <- monkey$"6"$item[j]^2
+      while (monkey$"6"$item[j] >= prod) {
+        monkey$"6"$item[j] <- monkey$"6"$item[j] %% prod
+      }
       monkey$"6"$count <- monkey$"6"$count + 1
       if (monkey$"6"$item[j] %% 17 == 0) {
         monkey$"1"$item <- c(monkey$"1"$item, monkey$"6"$item[j])
@@ -217,6 +238,9 @@ for (i in 1:10000) {
   if (length(monkey$"7"$item) > 0) {
     for (j in 1:length(monkey$"7"$item)) {
       monkey$"7"$item[j] <- monkey$"7"$item[j] + 6
+      while (monkey$"7"$item[j] >= prod) {
+        monkey$"7"$item[j] <- monkey$"7"$item[j] %% prod
+      }
       monkey$"7"$count <- monkey$"7"$count + 1
       if (monkey$"7"$item[j] %% 3 == 0) {
         monkey$"2"$item <- c(monkey$"2"$item, monkey$"7"$item[j])
